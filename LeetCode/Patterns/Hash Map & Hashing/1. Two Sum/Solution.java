aff555cf53d424b79b225421fb030786141abe21
Pranoti Varpe <121884358+pranotivarpe@users.noreply.github.com>
2026-08-21 12:31:15 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       HashMap<Integer,Integer> numMap = new HashMap<>();
-        int n = nums.length;
-
-        for(int i=0;i<n;i++){
-            int complement = target - nums[i];
-            if(numMap.containsKey(complement)){
-                return new int[]{numMap.get(complement),i};
+        for(int i = 0 ; i< nums;i++){
+            for(int j = 0;j< nums;j++){
+                if(nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                };
             }
-            numMap.put(nums[i], i);
         }
         return new int[]{};
     }
